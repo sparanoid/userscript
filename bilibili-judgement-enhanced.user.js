3be0676d56c1dc9ea8f12aff42612588a30895a9
sparanoid <t@sparanoid.com>
2022-01-07 07:13:38 -0500

    fix(bilibili-judgement-enhanced): wrong script scope

--- bilibili-judgement-enhanced.user.js
+++ bilibili-judgement-enhanced.user.js
@@ -2,7 +2,7 @@
 // @name         bilibili 风纪委员增强
 // @namespace    https://github.com/sparanoid/userscript
 // @supportURL   https://github.com/sparanoid/userscript/issues
-// @version      0.0.1
+// @version      0.0.2
 // @description  bilibili 风纪委员自动勾选表单，默认匿名发布
 // @author       Sparanoid
 // @license      AGPL
@@ -10,28 +10,44 @@
 // @compatible   edge 80 or later
 // @compatible   firefox 74 or later
 // @compatible   safari 13.1 or later
-// @match        https://www.bilibili.com/judgement/case-detail/*
+// @match        https://www.bilibili.com/judgement/*
 // @icon         https://experiments.sparanoid.net/favicons/v2/www.bilibili.com.ico
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
-window.addEventListener('load', () => {
-  const DEBUG = true;
-  const NAMESPACE = 'bilibili-judgement-enhanced';
-  const apiBase = 'https://api.bilibili.com';
-  const feedbackUrl = 'https://t.bilibili.com/545085157213602473';
+const DEBUG = true;
+const NAMESPACE = 'bilibili-judgement-enhanced';
+const apiBase = 'https://api.bilibili.com';
+const feedbackUrl = 'https://t.bilibili.com/545085157213602473';
 
-  console.log(`${NAMESPACE} loaded`);
+console.log(`${NAMESPACE} loaded`);
 
-  function debug(description = '', msg = '', force = false) {
-    if (DEBUG || force) {
-      console.log(`${NAMESPACE}: ${description}`, msg)
-    }
+function debug(description = '', msg = '', force = false) {
+  if (DEBUG || force) {
+    console.log(`${NAMESPACE}: ${description}`, msg)
   }
+}
 
-  document.querySelector('.vote-btns .btn-group button:first-child')?.click();
-  document.querySelector('.will-you-watch button:last-child')?.click();
-  document.querySelector('.vote-anonymous > div')?.click();
+((open) => {
+  XMLHttpRequest.prototype.open = function() {
+    this.addEventListener("readystatechange", () => {
+      const url = this?.responseURL && new URL(this.responseURL);
+      // debug('url?.pathname', url?.pathname);
 
-}, false);
+      // Check when vote API fires
+      if (url?.pathname === '/x/credit/v2/jury/vote') {
+
+        if (this.readyState === 4) {
+          document.querySelector('.vote-btns .btn-group button:first-child')?.click();
+          document.querySelector('.will-you-watch button:last-child')?.click();
+          document.querySelector('.vote-anonymous > div')?.click();
+          if (document.querySelector('.card-public-point')) {
+            document.querySelector('.card-public-point > div:last-child').style.display = 'block';
+          }
+        }
+      }
+    }, false);
+    open.apply(this, arguments);
+  };
+})(XMLHttpRequest.prototype.open);
