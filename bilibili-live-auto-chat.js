9b02c8b01cf010e096c47c0b95fd3d06051ff5d2
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2025-10-10 13:08:27 -0700

    Update version of bilibili-live-auto-chat.js to 2.0.1, reflecting minor improvements and adjustments.

--- bilibili-live-auto-chat.js
+++ bilibili-live-auto-chat.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LAPLACE 弹幕助手 - 哔哩哔哩直播间独轮车、弹幕发送
 // @namespace    https://greasyfork.org/users/9967
-// @version      2.0.0
+// @version      2.0.1
 // @description  这是 bilibili 直播间简易版独轮车，基于 quiet/thusiant cmd 版本 https://greasyfork.org/scripts/421507 继续维护而来
 // @author       laplace-live
 // @license      AGPL
