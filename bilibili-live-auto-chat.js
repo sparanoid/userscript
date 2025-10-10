c7168ff9c884e186c3f40b04c1c0a19b976249d3
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2025-10-10 13:07:18 -0700

    Add favicon icon URL to bilibili-live-auto-chat.js for improved branding and user recognition.

--- bilibili-live-auto-chat.js
+++ bilibili-live-auto-chat.js
@@ -5,6 +5,7 @@
 // @description  这是 bilibili 直播间简易版独轮车，基于 quiet/thusiant cmd 版本 https://greasyfork.org/scripts/421507 继续维护而来
 // @author       laplace-live
 // @license      AGPL
+// @icon         https://www.bilibili.com/favicon.ico
 // @match        *://live.bilibili.com/*
 // @grant        GM_setValue
 // @grant        GM_getValue
